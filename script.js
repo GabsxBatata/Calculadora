c594f9ca942a336c7d2a8bb3625270756c6cefb3
Carlos Gabriel Lima <carlosgabriel.limas27@gmail.com>
2022-10-17 03:40:31 -0300

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function calcular(tipo, valor) {    
     if (tipo === 'acao' ) {
-
+        
         if (valor === 'c') {
             //limpar visor
             document.getElementById('resultado').value = ''
         }
+        
         if (valor === '+' || valor === '-' || valor === '/' || valor === '*' || valor === '.') {
             document.getElementById('resultado').value += valor
         }
+        
         if (valor === '=') {
             let valor_campo = eval(document.getElementById('resultado').value)
             document.getElementById('resultado').value = valor_campo
